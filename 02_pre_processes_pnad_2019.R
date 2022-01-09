dbae7899d42957eef469d3eab9324aba424486b6
Marcio Colazingari <colazingari@anatel.gov.br>
2022-01-09 02:39:41 -0300

    Pre process Pnad construction

--- 02_pre_processes_pnad_2019.R
+++ 02_pre_processes_pnad_2019.R
@@ -4,4 +4,27 @@ library(tidyverse)
 # Carregamento dos dados do Pnad 2019
 pnad_2019 = get_pnadc(year = 2019, design = FALSE, interview = 1)
 
-#
+# Dicionário
+# - UF - Unidade da Federação
+# VD4019 - Rendimento Mensal Efetivo
+
+# Média das Remunerações po UF:
+remuneracao_uf_mean_sd = pnad_2019 %>%
+  select(VD4019, UF) %>%
+  group_by(UF) %>%
+  summarise(remuneracao_media = round(mean(VD4019, na.rm = TRUE), digits = 2),
+            desvio_padrao = round( sqrt(var(VD4019, na.rm = TRUE)),digits = 2  )) %>%
+  data.frame()
+
+# Alterar os dados dos municípios
+list_municipio = remuneracao_uf_mean_sd$UF 
+remuneracao_uf_mean_sd_renamed = remuneracao_uf_mean_sd %>%
+  mutate(UF = recode(UF,
+                     "Rondônia" = "RO",
+                     "Acre" = "AC",
+                     "Amazonas" = "AM",
+                     "Roraima" = "RR",
+                     "Pará" = "PA",
+                     "Amapá" = "AP",
+                     "Tocantins" = "TO",
+                     "Maranhão" = "MA"))
